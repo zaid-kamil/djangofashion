cf29e2e0606f5d43673f65c3c8886568c894d4ee
Zaid Kamil <xaidmetamorphos@gmail.com>
2024-03-21 11:02:20 +0530

    created accounts app and migrated data

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('about', TemplateView.as_view(template_name='about.html'), name='about'),
 ]
 
 if settings.DEBUG:
