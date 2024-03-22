2676cb783b2729cf528589339bf537656c6871b7
Zaid Kamil <xaidmetamorphos@gmail.com>
2024-03-22 11:17:55 +0530

    authentication #1

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("c/login", views.customer_login, name="clogin"),
+    path("s/login", views.seller_login, name="slogin"),
+    path("c/register", views.customer_register, name="cregister"),
+    path("s/register", views.seller_register, name="sregister"),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def customer_login(request):
+    return render(request, 'accounts/customer/login.html')
+def seller_login(request):
+    return render(request, 'accounts/seller/login.html')
+def customer_register(request):
+    return render(request, 'accounts/customer/register.html')
+def seller_register(request):
+    return render(request, 'accounts/seller/register.html')
