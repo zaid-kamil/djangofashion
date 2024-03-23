f745ca5ee90b84bc5cd4b0f28a07fdcbf00f392f
Zaid Kamil <xaidmetamorphos@gmail.com>
2024-03-23 11:06:07 +0530

    register logic

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def customer_login(request):
@@ -6,6 +9,26 @@ def customer_login(request):
 def seller_login(request):
     return render(request, 'accounts/seller/login.html')
 def customer_register(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        pwd1 = request.POST['password1']
+        pwd2 = request.POST['password2']
+        group = request.POST['group']
+        if pwd1 == pwd2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already exists')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'Email already exists')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=pwd1)
+                user.save()
+                group = Group.objects.get(name=group)
+                user.groups.add(group)
+                messages.success(request, 'Account successfully created')
+        else:
+            messages.error(request, 'Passwords do not match')
     return render(request, 'accounts/customer/register.html')
 def seller_register(request):
+    
     return render(request, 'accounts/seller/register.html')
