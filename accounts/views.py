941e5cbbb79d7ebd4cf4907b142a38eeb4ec9947
Zaid Kamil <xaidmetamorphos@gmail.com>
2024-03-23 11:16:39 +0530

    login

--- accounts/views.py
+++ accounts/views.py
@@ -30,5 +30,23 @@ def customer_register(request):
             messages.error(request, 'Passwords do not match')
     return render(request, 'accounts/customer/register.html')
 def seller_register(request):
-    
+    if request.method == "POST":
+        username = request.POST['username']
+        email = request.POST['email']
+        pwd1 = request.POST['password1']
+        pwd2 = request.POST['password2']
+        group = request.POST['group']
+        if pwd1 == pwd2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Username already exists')
+            elif User.objects.filter(email=email).exists():
+                messages.error(request, 'Email already exists')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=pwd1)
+                user.save()
+                group = Group.objects.get(name=group)
+                user.groups.add(group)
+                messages.success(request, 'Account successfully created')
+        else:
+            messages.error(request, 'Passwords do not match')
     return render(request, 'accounts/seller/register.html')
